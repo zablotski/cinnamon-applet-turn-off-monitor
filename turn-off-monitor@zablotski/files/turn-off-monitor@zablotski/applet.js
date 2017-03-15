3366f8147c2c5a8607e18cc48addbf181849a609
Yauheni Zablotski <e.zablotski@gmail.com>
2017-03-15 16:25:49 +0100

    exmple menu added

--- turn-off-monitor@zablotski/files/turn-off-monitor@zablotski/applet.js
+++ turn-off-monitor@zablotski/files/turn-off-monitor@zablotski/applet.js
@@ -1,9 +1,15 @@
 const Applet = imports.ui.applet;
 const Util = imports.misc.util;
 const GLib = imports.gi.GLib;
+const PopupMenu = imports.ui.popupMenu;
+const Mainloop = imports.mainloop;
 
 const APPLET_PATH = GLib.get_home_dir() + "/.local/share/cinnamon/applets/turn-off-monitor@zablotski";
 const ICON_PATH = APPLET_PATH + "/icon.png";
+
+const setTimeout = function (timeout, callback) {
+    Mainloop.timeout_add(timeout, callback);
+};
 
 function MyApplet(orientation, panel_height, instance_id) {
     this._init(orientation, panel_height, instance_id);
@@ -17,13 +23,34 @@ MyApplet.prototype = {
 
         this.set_applet_icon_path(ICON_PATH);
         this.set_applet_tooltip(_("Turn off monitor"));
+
+        this.menuManager = new PopupMenu.PopupMenuManager(this);
+        this.menu = new Applet.AppletPopupMenu(this, orientation);
+        this.menuManager.addMenu(this.menu);
+
+
+        this.menu.addAction(_("Turn off after 5 sec"), setTimeout(5000, turnOffMonitor));
+
+
+        this.menu.addAction(_("Open Web Dir"), function(event) {
+            Util.spawn("gedit ~/txt".split(' '));
+
+        });
     },
 
     on_applet_clicked: function() {
-        Util.spawn('xset dpms force off'.split(' '));
+        this.menu.toggle();
+        setTimeout(1000, turnOffMonitor);
+        // turnOffMonitor();
     }
+
+
 };
 
 function main(metadata, orientation, panel_height, instance_id) {
     return new MyApplet(orientation, panel_height, instance_id);
+}
+
+function turnOffMonitor() {
+    Util.spawn('xset dpms force off'.split(' '));
 }
