1e5b2f741d98e3faba701a7e99e94f4270f8a07f
Yauheni Zablotski <e.zablotski@gmail.com>
2018-10-03 01:40:18 +0200

    doc for scripts

--- copy_to_applets_repo.sh
+++ copy_to_applets_repo.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+#
+# Require clonned ../cinnamon-spices-applets/ repo
+# Updates ../cinnamon-spices-applets/ with files from current repo
+#
 rm -rf ../cinnamon-spices-applets/turn-off-monitor@zablotski/
 cp -r turn-off-monitor@zablotski/ ../cinnamon-spices-applets/
--- debug.sh
+++ debug.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+#
+# Removes old applet files from current OS with files from current repo
+#
 rm -rf ~/.local/share/cinnamon/applets/turn-off-monitor@zablotski
 cp -pr turn-off-monitor@zablotski/files/* ~/.local/share/cinnamon/applets/turn-off-monitor@zablotski
