5e0372f4b66651c98f3991b77c79e069a70211bc
Yauheni Zablotski <e.zablotski@gmail.com>
2018-10-03 01:43:27 +0200

    disable mouse for one second after applet clicked

--- copy_to_applets_repo.sh
+++ copy_to_applets_repo.sh
@@ -3,5 +3,4 @@
 # Require clonned ../cinnamon-spices-applets/ repo
 # Updates ../cinnamon-spices-applets/ with files from current repo
 #
-rm -rf ../cinnamon-spices-applets/turn-off-monitor@zablotski/
-cp -r turn-off-monitor@zablotski/ ../cinnamon-spices-applets/
+cp -ru turn-off-monitor@zablotski/ ../cinnamon-spices-applets/
